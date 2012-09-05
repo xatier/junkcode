e478a8a1626d36114d0f7dc0b8f3a7016d7d2abf
xatier <xatier@gmail.com>
2012-09-06 02:00:19 +0800

    ...

--- C/vet.c
+++ C/vet.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
-int f(int n){
-    int i,t=1;
-    for(i=n;i>0;i--)t=t*i;
-    return t;
+
+int main(){
+    int a,b,c,o,i,j;
+    int n=0;
+    while(scanf("%d %d",&i,&j)!=EOF){ 
+        printf("%d %d",i,j);
+
+        if(i>j){ 
+            b=i;
+            i=j;
+            j=b;    
+        } 
+        for(a=i;a<=j;a++){
+
+            c=a;    
+            o=0; 
+            while(1){
+                if(c==1){
+                    o++;    
+                    break;  
+                } 
+                if(c%2==1){
+                    c=c*3+1; 
+                    o++; 
+                } 
+                else if(c%2==0){
+                    c=c/2; 
+                    o++; 
+                }
+            }
+
+            if(o>n){
+                n=o; 
+            } 
+        }
+        printf(" %d\n",n);
+        n=0; 
+    } 
+
+    return 0; 
 }
-int c(int n,int k){
-    int t;
-    t=f(n)/(f(k)*f(n-k));
-    return t;
-}
-int main(void){
-    int n, k;
-    printf("Enter c(n,k), n>=k>=0:");
-    while(scanf("c ( %d , %d )",&n,&k)!=EOF){
-        getchar();
-        printf("c(%d,%d) = %d!/(%d!%d!) = %d/(%d*%d) = %d\n",n,k,n,k,n-k,f(n),f(k),f(n-k),c(n,k));
-        printf("Enter c(n,k), n>=k>=0:");
-    }
-}
+/*by Xatier*/
--- Cpp/test.cpp
+++ Cpp/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 struct foo {
     int bar;
@@ -12,6 +13,11 @@ int main (void) {
     foo *bar = new (foo);
     bar->bar = 20;
     funcc(*bar);
+    delete bar;
+    int a = 10;
+    ++++++++a;
+    std::cout << a;
+    std::cout << time(nullptr);
 
     return 0;
 }
