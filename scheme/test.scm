139d1cbac362858a9935b12c1b69e892aa1e2d96
xatier <xatierlike@gmail.com>
2012-11-13 01:52:07 +0800

    u2b.pl: a youtube search tool under command line

--- scheme/test.scm
+++ scheme/test.scm
@@ -1,14 +1,5 @@
 (boolean? #t)
 (boolean? "Hello, World!")
-(define (f x) x)
-(f 10)
 
-(define (remove x ls)
-  (let loop((ls0 ls) (ls1 ()))
-    (if (null? ls0) 
-    (reverse ls1)
-    (loop
-     (cdr ls0)
-          (if (eqv? x (car ls0))
-              ls1
-            (cons (car ls0) ls1))))))
+(define (pow x) 
+  if (= x 1) 1 1)
