72dc5b3330881fa17846282f6f6b0c1830833ad7
xatier <xatierlike@gmail.com>
2012-12-03 23:16:53 +0800

    foo

--- Perl/kerker.pl
+++ Perl/kerker.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl
 
-use List::Util qw (sum);
-@a = ( 1 .. 10);
-print sum @a;
+use 5.014;
+use Data::Dumper;
+
+my $r = 3;
+my $S = [[1, 2, 4]];
+
+
+say Dumper $S;
+say Dumper $S->[0][0];
